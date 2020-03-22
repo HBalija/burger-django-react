055daa26e4a38f4463c242501ed15bbd6ff67129
HBalija <balija.hrvoje@gmail.com>
2020-03-22 17:27:09 +0100

    Add ingredient state to burger builder and dynamically render them

--- client/src/components/Burger/Burger.js
+++ client/src/components/Burger/Burger.js
@@ -4,12 +4,29 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 import './Burger.scss';
 
-const burger = () => {
+const burger = props => {
+  // convert object to array of keys (array of arrays)
+  const transformedIngredients = Object.keys(props.ingredients)
+    .map(igKey => {
+      return [...Array(props.ingredients[igKey])]
+        .map((_, idx) => {
+          return <BurgerIngredient key={igKey + idx} type={igKey} />;
+        });
+    });
+
+  // convert to array of elements so length can be calculated
+  let reducedIngredients = transformedIngredients.reduce((arr, el) => {
+    return arr.concat(el);
+  }, []);
+
+  if (!reducedIngredients.length) {
+    reducedIngredients = <p>Please start adding ingredients</p>;
+  }
+
   return (
     <div className="burger">
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {reducedIngredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
--- client/src/containers/BurgerBuilder/BurgerBuilder.js
+++ client/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,11 +2,22 @@ import React, { Component } from 'react';
 
 import Burger from '../../components/Burger/Burger';
 
+
 export default class BurgerBuilder extends Component {
+
+  state = {
+    ingredients: {
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0
+    }
+  }
+
   render() {
     return (
       <>
-        <Burger />
+        <Burger ingredients={this.state.ingredients} />
         <div>Build controls</div>
       </>
     );
