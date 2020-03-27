496ae23dba0baabe71f917829280db8f2d1bd1a5
HBalija <balija.hrvoje@gmail.com>
2020-03-27 20:09:51 +0100

    Add auth container, route, form and form validation

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import Layout from './hoc/Layout/Layout';
+import AuthContainer from './containers/AuthContainer/AuthContainer';
 import BurgerContainer from './containers/BurgerContainer/BurgerContainer';
 import CheckoutContainer from './containers/CheckoutContainer/CheckoutContainer';
 import OrdersContainer from './containers/OrdersContainer/OrdersContainer';
@@ -12,9 +13,10 @@ function App() {
     <div>
       <Layout>
         <Switch>
+          <Route component={BurgerContainer} exact path="/" />
           <Route component={CheckoutContainer} path="/checkout" />
           <Route component={OrdersContainer} path="/orders" />
-          <Route component={BurgerContainer} exact path="/" />
+          <Route component={AuthContainer} path="/auth" />
         </Switch>
       </Layout>
     </div>
